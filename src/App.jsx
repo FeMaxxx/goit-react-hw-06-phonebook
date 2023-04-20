d5b8c33747b688cc853b75b993447be615341e90
FeMaxxx <119453050+FeMaxxx@users.noreply.github.com>
2023-04-20 19:31:28 +0300

    ff

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 import React from "react";
 
-import { PhoneBook } from "components/PhoneBook";
-import { PBForm } from "components/PBForm";
-import { ContactFilter } from "components/ContactFilter";
-import { ContactList } from "components/ContactList";
-import { Contacts, Title } from "components/Contacts";
+// import { PhoneBook } from "components/PhoneBook";
+// import { PBForm } from "components/PBForm";
+// import { ContactFilter } from "components/ContactFilter";
+// import { ContactList } from "components/ContactList";
+// import { Contacts, Title } from "components/Contacts";
 
 const App = () => {
   return (
     <>
-      <PhoneBook title="PhoneBook">
+      <p>asdasda</p>
+      {/* <PhoneBook title="PhoneBook">
         <PBForm />
         <Contacts>
           <Title>Contacts</Title>
           <ContactFilter />
           <ContactList />
         </Contacts>
-      </PhoneBook>
+      </PhoneBook> */}
     </>
   );
 };
